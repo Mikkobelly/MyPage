948fc22d3bbf216df186d4ba49a2e1a2294b9e8d
mikia <mikiakuta0529@gmail.com>
2022-12-01 11:50:57 +0000

    Initial commit

---
+++ docs/precache-manifest.e0d7a77b13865d51d75ffdcea3990296.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "2687e18e5e2ec8d84ddd",
+    "url": "/My-page/static/css/main.bd11664c.chunk.css"
+  },
+  {
+    "revision": "2687e18e5e2ec8d84ddd",
+    "url": "/My-page/static/js/main.2687e18e.chunk.js"
+  },
+  {
+    "revision": "361df6b790a3761d5106",
+    "url": "/My-page/static/js/1.361df6b7.chunk.js"
+  },
+  {
+    "revision": "02928874e6c24efe4b3f",
+    "url": "/My-page/static/js/runtime~main.02928874.js"
+  },
+  {
+    "revision": "2db4f60983da43bfe4c504c2ff9e3c37",
+    "url": "/My-page/static/media/header.2db4f609.jpg"
+  },
+  {
+    "revision": "76caa6c22276518a9ef9f5376b19c1c6",
+    "url": "/My-page/static/media/headerMobile.76caa6c2.jpg"
+  },
+  {
+    "revision": "a09fe01ddd7906f5f87b8a2b2358663b",
+    "url": "/My-page/static/media/bioImg.a09fe01d.jpg"
+  },
+  {
+    "revision": "00b42b86d8e65eb44fda30551a4ab9c9",
+    "url": "/My-page/static/media/swan.00b42b86.jpg"
+  },
+  {
+    "revision": "c7b5b3eead25631422d77896f17454d9",
+    "url": "/My-page/static/media/yelpcamp.c7b5b3ee.png"
+  },
+  {
+    "revision": "053a4d7616761ad318b9494fb1482840",
+    "url": "/My-page/static/media/ToDoList.053a4d76.png"
+  },
+  {
+    "revision": "f25df19f239090fa90150625a92ffdcf",
+    "url": "/My-page/static/media/KeeperApp.f25df19f.png"
+  },
+  {
+    "revision": "67e9c8c94d50f4fe43cf120f9661091a",
+    "url": "/My-page/static/media/whacamole.67e9c8c9.png"
+  },
+  {
+    "revision": "89a3ad4bfa6a24861e63e7164f3eb721",
+    "url": "/My-page/static/media/memorygame.89a3ad4b.png"
+  },
+  {
+    "revision": "4ca00c2140cb6da6cbb29417654d0ff3",
+    "url": "/My-page/static/media/contact.4ca00c21.jpg"
+  },
+  {
+    "revision": "285d45f7fee0b76a94f41ac78128bbe5",
+    "url": "/My-page/static/media/contactMobile.285d45f7.jpg"
+  },
+  {
+    "revision": "f770f4d3a08e95066309be1a1afbb7bc",
+    "url": "/My-page/static/media/background5.f770f4d3.jpeg"
+  },
+  {
+    "revision": "2a82fbababc449a87ca19be6190d6a7f",
+    "url": "/My-page/static/media/background7.2a82fbab.jpg"
+  },
+  {
+    "revision": "79a0c02124dbcf9d3f5db3ce29dec765",
+    "url": "/My-page/static/media/background4.79a0c021.jpeg"
+  },
+  {
+    "revision": "428989cdc870af142325de51821bab58",
+    "url": "/My-page/static/media/background1.428989cd.jpeg"
+  },
+  {
+    "revision": "46fd64e98e295bb9b114311c6bfb543d",
+    "url": "/My-page/static/media/background3.46fd64e9.jpeg"
+  },
+  {
+    "revision": "323674be4d2266b4d99e71d7d26dbde8",
+    "url": "/My-page/static/media/background8.323674be.jpg"
+  },
+  {
+    "revision": "e98c5678ada4150dc0c2310fc6cbf68f",
+    "url": "/My-page/index.html"
+  }
+];
